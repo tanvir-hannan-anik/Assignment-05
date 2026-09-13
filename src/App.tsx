60cff95962959ffcc7746e2f0d19e60ce6f755de
tanviranik12 <251-16-021@diu.edu.bd>
2026-09-13 23:20:55 +0600

    Final Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "./component/HeroSection";
 import Nav from "./component/Nav";
 import TechnologySection from "./component/TechnologySection";
-import "./App.css";
 import Footer from "./component/Footer";
 
 function App() {
